03ccd098f13c702a0886538a87b66c8906328912
littleonetwo <littleonetwo@gmail.com>
2019-09-20 20:22:21 -0500

    initial upload with most content complete, style sheets about 80%

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+/* add the logic here for a switch that will toggle the theme of the site*/
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {        document.documentElement.setAttribute('data-theme', 'light');
+          localStorage.setItem('theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
